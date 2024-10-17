406079d11fe018db76b5c5b8f76d8cabac923a31
Seedrahelal <sedeahelal16@gmail.com>
2024-10-17 19:45:31 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16), color: Colors.amber[200]),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-                title: const Text('Flutter Tips',
-                    style: TextStyle(color: Colors.black, fontSize: 26)),
-                subtitle: Padding(
-                  padding: const EdgeInsets.only(top: 16, bottom: 16),
-                  child: Text('Build your Career with Tharwat samy',
-                      style: TextStyle(
-                          color: Colors.black.withOpacity(.5), fontSize: 18)),
-                ),
-                trailing: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      FontAwesomeIcons.trash,
-                      color: Colors.black,
-                      size: 33,
-                    ))),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text('May 21,2022',
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(.4), fontSize: 16)),
-            )
-          ],
-        ));
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.amber[200]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                  title: const Text('Flutter Tips',
+                      style: TextStyle(color: Colors.black, fontSize: 26)),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 16),
+                    child: Text('Build your Career with Tharwat samy',
+                        style: TextStyle(
+                            color: Colors.black.withOpacity(.5), fontSize: 18)),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        FontAwesomeIcons.trash,
+                        color: Colors.black,
+                        size: 33,
+                      ))),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
+                child: Text('May 21,2022',
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(.4), fontSize: 16)),
+              )
+            ],
+          )),
+    );
   }
 }
